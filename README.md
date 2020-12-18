1bfa3a878c09a332505bf206b8b5528ab4468ad2
Chris <chris.tisserat@gmail.com>
2020-12-17 16:51:30 -0800

    first commit classes created working on methods

---
+++ README.md
@@ -0,0 +1,47 @@
+# Project Name
+
+> Codecademy Project JavaScript CLass Project: School-Catalogue;
+
+## Table of contents
+
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Launch](#launch)
+* [Features](#features)
+* [Status](#status)
+* [Inspiration](#inspiration)
+* [Contact](#contact)
+
+## General info
+
+Project Requirements
+
+* Project Prompt - Let’s put your knowledge of classes to the test by creating a digital school catalog for the New York City Department of Education  
+* Classes and sub-classes need to be built as described following challenge of creating classes and sub-classes then follow steps 5-7 to finish project.
+* Test the script as needed.
+
+## Technologies
+
+* JavaScript
+
+## Launch
+
+node app.js (with node.js installed)
+
+## Features
+
+* Create classes and sub-classes
+* Create instances
+* Call methods
+
+## Status
+
+Project is: In Progress
+
+## Inspiration
+
+Codecademy Project JavaScript CLass Project: School-Catalogue;
+
+## Contact
+
+Created by KodiBearGit
---
+++ app.js
@@ -0,0 +1,56 @@
+class School {
+  constructor(name, level, numberOfStudents) {
+    this._name = name;
+    this._level = level;
+    this._numberOfStudents = numberOfStudents;
+  }
+  get name() {
+    return this._name;
+  }
+  get level() {
+    return this._level;
+  }
+  get numberOfStudents() {
+    return this._numberOfStudents;
+  }
+  set numberOfStudents(number) {
+    if (typeof(number) === 'number') {
+     this._numberOfStudents = number;
+    } else {
+    console.log("Invalid input: numberOfStudents must be set to a Number.")
+    }
+  }
+  quickFacts() {
+    console.log($(this._name) ` educates ` $(this._numberOfStudents) ` students at the ` $(this._level) ` school level.`);
+  }
+  static pickSubstituteTeacher(substuteTeachers) {
+    let ranNum = Math.floor(Math.random() * subtituteTeachers.length - 1);
+    return substi
+  }
+}
+
+class Primary extends School {
+  constructor(name, level, numberOfStudents) {
+    super(name, level, numberOfStudents);
+    this._pickupPolicy = '';
+  }
+  get pickupPolicy() {
+    return this._pickupPolicy;
+  }
+}
+
+class Middle extends School {
+  constructor(name, level, numberOfStudents) {
+    super(name, level, numberOfStudents);
+  }
+}
+
+class High extends School {
+  constructor(name, level, numberOfStudents) {
+    super(name, level, numberOfStudents);
+    this._sportsTeams = [];
+  }
+  get sportsTeams() {
+    return this._sportsTeams;
+  }
+}
